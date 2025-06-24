bc18bee35dd4286d968479fca0a10f45f0bee5d2
DRickard <drickard1967@library.ucla.edu>
2025-06-24 10:50:35 -0700

    [SYS-1877] initial coding

---
+++ src/edu/ucla/library/libservices/webservices/ecommerce/web/clients/XeroClient.java
@@ -0,0 +1,105 @@
+package edu.ucla.library.libservices.webservices.ecommerce.web.clients;
+
+import com.google.gson.Gson;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+import edu.ucla.library.libservices.webservices.ecommerce.beans.XeroContact;
+
+import org.apache.log4j.Logger;
+
+public class XeroClient
+{
+  private static final Logger LOGGER = Logger.getLogger(XeroClient.class);
+
+  private Client client;
+  private WebResource webResource;
+  private String accessToken;
+  private String resourceURI;
+  private String tenantID;
+  private String uriBase;
+  private String userID;
+  private XeroContact theContact;
+
+  public XeroClient()
+  {
+    super();
+  }
+
+  public void setAccessToken(String accessToken)
+  {
+    this.accessToken = accessToken;
+  }
+
+  public String getAccessToken()
+  {
+    return accessToken;
+  }
+
+  public void setResourceURI(String resourceURI)
+  {
+    this.resourceURI = resourceURI;
+  }
+
+  public String getResourceURI()
+  {
+    return resourceURI;
+  }
+
+  public void setTenantID(String tenantID)
+  {
+    this.tenantID = tenantID;
+  }
+
+  public String getTenantID()
+  {
+    return tenantID;
+  }
+
+  public void setUriBase(String uriBase)
+  {
+    this.uriBase = uriBase;
+  }
+
+  public String getUriBase()
+  {
+    return uriBase;
+  }
+
+  public void setUserID(String userID)
+  {
+    this.userID = userID;
+  }
+
+  public String getUserID()
+  {
+    return userID;
+  }
+
+  public XeroContact getTheContact()
+  {
+    if (theContact == null)
+    {
+      ClientResponse response;
+      client = Client.create();
+      webResource = client.resource(getUriBase().concat(getUserID()));
+      
+      String authString = "Bearer ".concat(getAccessToken());
+
+      response = webResource.accept("application/json").header("Authorization", authString).
+                header("xero-tenant-id", getTenantID()).get(ClientResponse.class);
+      if (response.getStatus() == 200)
+      {
+        String json = response.getEntity(String.class);
+        theContact = new Gson().fromJson(json, XeroContact.class);
+      }
+      else
+      {
+        theContact = new XeroContact();
+      }
+    }
+    return theContact;
+  }
+}
