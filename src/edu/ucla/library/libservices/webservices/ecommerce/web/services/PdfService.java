637f23737fa1d8bc19b40d9e85f6e6396f327af6
DRickard <drickard1967@library.ucla.edu>
2025-09-26 11:21:17 -0700

    [SYS-2007_8] remove redundant code

--- src/edu/ucla/library/libservices/webservices/ecommerce/web/services/PdfService.java
+++ src/edu/ucla/library/libservices/webservices/ecommerce/web/services/PdfService.java
@@ -40,7 +40,6 @@ public class PdfService
 
     pdfClient = new XeroInvoiceClient();
     pdfClient.setInvoiceID(invoiceNo);
-    pdfClient.setPort(0);
     pdfClient.setSecretsFile(config.getServletContext().getInitParameter("xero.secrets"));
     pdfClient.setTokensFile(config.getServletContext().getInitParameter("xero.tokens"));
     return Response.ok( pdfClient.getInvoicePDF().getEntityInputStream() ).header("Content-Disposition", "inline; filename=".concat(filename)).build();
